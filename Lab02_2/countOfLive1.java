33dbce3b8b7d6e595e7b1d4fb0359360fea30313
Natallia Vasilyeva <32133652+NatalliaVasilyeva@users.noreply.github.com>
2018-02-16 09:45:42 +0000

    Update countOfLive1.java

--- Lab02_2/countOfLive1.java
+++ Lab02_2/countOfLive1.java
@@ -1,7 +1,10 @@
+/* Необходимо ввести информацию о дате рождения.
+   После чего программа посчитает, сколько лет, месяцев, дней, минут и секунд Вы прожили */
+
+// подключаем необходимые библиотека для обработки дат, ввода информации с клавиатуры
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
 import java.util.Scanner;
 import java.text.ParseException;
 
@@ -12,19 +15,18 @@ public class countOfLive1 {
     public static void main(String[] args) {
 
 	
-	System.out.println("Input your date of birth in format dd.MM.yyyy HH.mm.ss");
-	
+	System.out.println("Input your date of birth in format dd.MM.yyyy HH.mm.ss");// просьба ввести информацию о дате рождения в определенном формате
         Scanner scan = new Scanner(System.in);
-        String date = scan.nextLine();
+        String date = scan.nextLine(); // внесение информации о дате рождения в строковую переменную date
         
-        DateFormat format = new SimpleDateFormat("dd.MM.yyyy HH.mm.ss");
+        DateFormat format = new SimpleDateFormat("dd.MM.yyyy HH.mm.ss"); // определяем формат представления даты
         try {
-            Date parseDate = format.parse(date);
-            Date nowdate = new Date();
-            long substruction = Math.abs(nowdate.getTime()-parseDate.getTime());
-	        System.out.println(substruction); 
-     
-	 
+            Date parseDate = format.parse(date); // преобразовываем строчку с датой рождения в формат date
+            Date nowdate = new Date(); // оздаем новую переменную для определения текущей даты
+            long substruction = Math.abs(nowdate.getTime()-parseDate.getTime()); // количество прожитых миллисекунд
+	        
+	// вывод информации на экран о прожитых годах, месяцах, днях, минутах и секундах	
+    System.out.println(substruction); 
     System.out.println("Your life in second: " + substruction/1000);
     System.out.println("Your life in minute: " + substruction/1000/60);
     System.out.println("Your life in hour: " + substruction/1000/60/60);
@@ -34,11 +36,6 @@ public class countOfLive1 {
         }
         catch(ParseException parseEx){
         parseEx.printStackTrace();
-    }
-
-
-              
-               
-    
-    }
+    }        
+   }
 }
